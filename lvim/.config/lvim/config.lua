cf50e318790091f489a34086b62e48a93ca61992
Matheus Henrique <matheus.hpp135@gmail.com>
2022-10-05 10:54:56 -0300

    initial commit

---
+++ lvim/.config/lvim/config.lua
@@ -0,0 +1,186 @@
+-- general neovim
+
+-- clipboard configuration
+local has = vim.fn.has
+local is_win = has "win32"
+
+if is_win then
+  vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
+else
+  vim.opt.clipboard:append { 'unnamedplus' }
+end
+
+vim.opt.relativenumber = true
+
+-- general lunarvim
+
+lvim.log.level = "warn"
+lvim.format_on_save = false
+
+-- Change theme settings
+lvim.colorscheme = "tokyonight"
+lvim.transparent_window = true
+-- lvim.builtin.theme.options.dim_inactive = true
+-- lvim.builtin.theme.options.style = "storm"
+
+lvim.leader = "space"
+lvim.keys.normal_mode["<leader>w"] = ":w<cr>"
+lvim.keys.normal_mode["<leader>q"] = ":q<cr>"
+lvim.keys.normal_mode["<leader>vs"] = ":vsplit<cr>"
+lvim.keys.normal_mode["<leader>hs"] = ":split<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.insert_mode["kj"] = "<Esc>"
+-- Delete a word backwards
+lvim.keys.normal_mode["db"] = 'vb"_d'
+-- Better paste
+lvim.keys.visual_mode["p"] = '"_dP'
+-- '-' to end of line
+lvim.keys.normal_mode["-"] = "$"
+lvim.keys.visual_mode["-"] = "$"
+
+-- Override telescope defaults
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  n = {
+    ["q"] = actions.close
+  },
+  i = {
+    ["<Down>"] = actions.cycle_history_next,
+    ["<Up>"] = actions.cycle_history_prev,
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  }
+}
+
+lvim.builtin.which_key.mappings["f"] = {
+  name = "+Telescope",
+  f = { "<cmd>Telescope find_files<CR>", "Find Files" },
+  t = { "<cmd>Telescope live_grep<CR>", "Search Text" },
+  p = { "<cmd>Telescope projects<CR>", "Projects" },
+  b = { "<cmd>Telescope buffers<CR>", "Show Buffers"}
+}
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
+
+-- generic LSP settings
+
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+-- lvim.lsp.installer.setup.ensure_installed = {
+--     "sumeko_lua",
+--     "jsonls",
+-- }
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     uninstall_server = "d",
+--     toggle_server_expand = "o",
+-- }
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.installer.setup.automatic_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "black", filetypes = { "python" } },
+--   { command = "isort", filetypes = { "python" } },
+--   {
+--     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "prettier",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--print-with", "100" },
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+
+-- -- set additional linters
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--severity", "warning" },
+--   },
+--   {
+--     command = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
+-- }
+
+-- Additional Plugins
+-- lvim.plugins = {
+--   {
+--     "NTBBloodbath/rest.nvim"
+--   },
+-- }
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.json", "*.jsonc" },
+--   -- enable wrap mode for json files only
+--   command = "setlocal wrap",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
---
+++ zsh/.config/zsh/.zshrc
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+setopt appendhistory                                      # Append history to the history file (no overwriting)
+setopt sharehistory                                       # Share history across terminals
+setopt incappendhistory                                   # Immediately append to the history file, not just when a term is killed
+
+# Some useful options (man zshoptions)
+setopt autocd extendedglob nomatch menucomplete
+setopt interactive_comments
+stty stop undef                                           # disable ctrl-s to freeze terminal
+zle_highlight=('paste: none')
+
+# beeping is annoying
+unsetopt BEEP
+
+# completions
+zstyle ':completion:*' menuselect
+zmodload zsh/complist
+# compinit
+_comp_options+=(globdots)                                 # include hidden files
+
+# if you press up or down during a part of a command, only the commands that start with that part will be shown
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+# colors
+autoload -Uz colors && colors
+
+# useful functions
+source "$ZDOTDIR/functions"
+
+# plugins
+zsh_add_plugin "zsh-users/zsh-autosuggestions"
+zsh_add_plugin "zsh-users/zsh-syntax-highlighting"
+zsh_add_plugin "zsh-users/zsh-completions"
+zsh_add_plugin "hlissner/zsh-autopair"
+zsh_add_plugin "spaceship-prompt/spaceship-prompt"
+
+# normal files to source
+zsh_add_file "history"
+zsh_add_file "aliases"
+zsh_add_file "plugins/spaceship-prompt/spaceship.zsh"
+zsh_add_file "prompt"
+
+# asdf
+. "$ASDF_DIR/asdf.sh"
+fpath=(${ASDF_DIR}/completions $fpath) # append asdf completions to fpath
+
+# completions
+fpath=(${ZDOTDIR}/plugins/zsh-completions/src $fpath)
+
+#z
+[[ -r "/usr/share/z/z.sh" ]] && source /usr/share/z/z.sh
+
+# load compinit for working completions
+zsh_add_file "compinit"
